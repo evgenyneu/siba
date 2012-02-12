cbdb996caa6741c6de9703ed5ce4e489a7ca2d98
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2012-02-12 16:26:18 +0400

    Added get_backup_list and restore to destination scaffold class

--- scaffolds/destination.rb
+++ scaffolds/destination.rb
@@ -15,6 +15,18 @@ module Siba::C6y
       def backup(path_to_backup_file) 
 ## examples.rb ##
       end
+
+      # Shows the list of backup files stored currently at destination
+      # with names starting with 'backup_name'
+      #
+      # Returns an array of two-element arrays:
+      # [backup_file_name, modification_time]
+      def get_backups_list(backup_name)
+      end
+
+      # Restoring: put backup file from destination into dir
+      def restore(backup_name, dir)
+      end
     end
   end
 end
