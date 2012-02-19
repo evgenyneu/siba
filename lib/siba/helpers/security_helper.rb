c9c88e48e3a7f5b9157fb08a7a77b5f3093fda62
Evgeny Neumerzhitskiy <evgenyneu@gmail.com>
2012-02-19 17:25:12 +0400

    Remove` character from password generator for YAML

--- lib/siba/helpers/security_helper.rb
+++ lib/siba/helpers/security_helper.rb
@@ -6,7 +6,7 @@ module Siba
   class SecurityHelper
     class << self
       def generate_password_for_yaml(length = 16)
-        characters = (32..126).to_a - "\\\"".bytes.to_a
+        characters = (32..126).to_a - "\\\"".bytes.to_a - "`".bytes.to_a
 
         (0...length).map{      
           characters[SecureRandom.random_number(characters.size)].chr
